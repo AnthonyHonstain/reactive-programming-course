5a8c6755412a785f2052b6d603f3d808861d5c92
Anthony Honstain <honstain@gmail.com>
2022-09-25 11:52:07 -0700

    Initial commit

---
+++ src/main/java/com/example/reactivedemo/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.example.reactivedemo;
+
+import com.example.reactivedemo.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+
+    Mono<Person> getById(Integer id);
+
+    Flux<Person> findAll();
+}
---
+++ src/main/java/com/example/reactivedemo/PersonRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.reactivedemo;
+
+import com.example.reactivedemo.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class PersonRepositoryImpl implements PersonRepository {
+
+    @Override
+    public Mono<Person> getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Flux<Person> findAll() {
+        return null;
+    }
+}
